0039a421104618b5279831698871af0c0e09f795
Ahmet Hatipoglu <ahmet4192@gmail.com>
2016-04-18 02:30:51 +0300

    Added react files

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.browserify('index.js', 'public/assets/js/app.js');
 });
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Jester Joke Recommender</title>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="/assets/js/app.js"></script>
+    </body>
+</html>
